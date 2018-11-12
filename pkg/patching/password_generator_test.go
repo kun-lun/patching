50b99256e131b28d1add94247562a86f10f5a21d
Andy Liu <andyliuliming@outlook.com>
2018-11-12 20:12:20 +0800

    patching for the manifest.

---
+++ pkg/patching/password_generator_test.go
@@ -0,0 +1,31 @@
+package patching_test
+
+import (
+	"github.com/kun-lun/patching/pkg/patching"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("PasswordGenerator", func() {
+
+	var (
+		generator patching.ValueGenerator
+	)
+
+	BeforeEach(func() {
+		generator = patching.NewPasswordGenerator()
+	})
+	Describe("Generate", func() {
+		Context("Everything OK", func() {
+			It("should succeed", func() {
+				params := map[string]int{
+					"length": 0,
+				}
+				password, err := generator.Generate(params)
+				Expect(err).To(BeNil())
+				Expect(password).NotTo(BeNil())
+				Expect(len(password.(string))).To(Equal(20))
+			})
+		})
+	})
+})
